88e733e97b2368fd5c846791d0967d6df495c652
Robert Phamle <rphamle@squareup.com>
2023-01-22 18:53:25 -0800

    fix docker setup

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ import WebpackDevServer from 'webpack-dev-server';
 import config from './webpack.local.config';
 
 new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
+  devMiddleware: {
+    publicPath: config.output.publicPath,
+  },
   port: 3000,
   hot: true,
-  inline: true,
   historyApiFallback: true,
   headers: { 'Access-Control-Allow-Origin': '*' },
 }).listen(3000, '0.0.0.0', (err) => {
